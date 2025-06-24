7cae7e5a2d5590e92bb2b36c244f604559d12332
Daniel Son <danielson@mac.lan>
2025-06-24 17:34:48 -0400

    added file writing

--- scanner.py
+++ scanner.py
@@ -19,7 +19,12 @@ def nmap_scan(target, scanner):
                     "State": port_data["state"],
                     "Method": f"{reason}"
                 })
-    return pd.DataFrame(results)
+    results = pd.DataFrame(results).to_string()
+
+    if not results:
+        return "No open ports detected.\n\n"
+    else:
+        return f"Nmap Scan Results for {target}:\n\n" + results + "\n"
 
 def os_detection_scan(target, scanner):
     try:
@@ -28,13 +33,17 @@ def os_detection_scan(target, scanner):
             guessed_os = result['scan'][target]['osmatch'][0]
             accuracy = guessed_os.get('accuracy', '?')
             name = guessed_os.get('name', 'Unknown OS')
-            print(f"\nThere is a {accuracy}% chance that the host is running {name}.\n")
+            return f"\nThere is a(n) {accuracy}% chance that the host is running {name}.\n\n"
         else:
-            print("\nOS detection failed or returned no results.\n")
+            return "\nOS detection failed or returned no results.\n\n"
     except Exception as e:
-        print(f"Error during OS detection: {e}")
+        return f"Error during OS detection: {e}\n\n"
+
+####################################################################################################
 
 if __name__ == "__main__":
+
+    to_file = input("\nWrite results to a file? (Y/N): ")
 
     print('\nRunning...\n')
 
@@ -43,18 +52,23 @@ if __name__ == "__main__":
 
     # initial status scan
     nm_scanner = nm_scan.scan(target, '5000', arguments='-Pn')
-    print(f"The host {target} is " + nm_scanner['scan'][target]['status']['state'] + "\n")
+    host_results = f"The host {target} is " + nm_scanner['scan'][target]['status']['state'] + "\n\n"
 
     # nmap scan
     scan_results = nmap_scan(target, nm_scan)
-    if scan_results.empty:
-        print("No open ports detected.\n")
-    else:
-        print(f"Nmap Scan Results for {target}:\n")
-        print(scan_results)
 
     # os detection scan
-    os_detection_scan(target, nm_scan)
+    os_results = os_detection_scan(target, nm_scan)
 
-    print("Report generated " + time.strftime("%Y-%m-%d_%H:%M:%S GMT", time.gmtime()))
+    # timestamp
+    timestamp = "Report generated " + time.strftime("%Y-%m-%d_%H:%M:%S GMT", time.gmtime())
+
+    #print results
+    print(host_results + scan_results + os_results + timestamp)
+
+    # write results to file
+    if to_file == 'Y':
+        with open(f"{sys.argv[1]}_scan.txt", 'w') as file:
+            file.write(host_results + scan_results + os_results + timestamp)
+
     print("\nFinished.\n")
