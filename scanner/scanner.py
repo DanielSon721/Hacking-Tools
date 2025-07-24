5e527044b282219be6c8d7f41c17ac2da34adedc
Daniel Son <danielson@MacBook-Pro-770.local>
2025-07-24 17:25:24 +0900

    minor fixes

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -76,9 +76,9 @@ if __name__ == "__main__":
     time.sleep(1)
     print("Passing results to Gemini AI...\n")
 
-    ai_results = ai_analysis("Analyze this nmap scan. Give me a quick summary, possible vulnerabilities, possible attack techniques, exploitation likelihood, and suggested remediation. Keep it simple. Less than 1200 characters.\n" + scan_results + os_results)
+    ai_results = ai_analysis("Analyze this nmap scan. Give me a quick summary, possible vulnerabilities, possible attack techniques, exploitation likelihood, and suggested remediation. Keep it simple. Less than 1200 characters.\n" + scan_results + os_results) + '\n'
 
-    print(ai_results)
+    print(ai_results, end = "")
 
     # timestamp
     time.sleep(1)
